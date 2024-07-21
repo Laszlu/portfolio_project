fe19aa2d165ae1ca9c09ace990d493766c9d5f32
Laszlu <maurice.ferreyra@gmail.com>
2024-07-21 13:55:26 +0200

    added navbar tab coloring depending on state

--- src/components/navbar/NavBarHome.tsx
+++ src/components/navbar/NavBarHome.tsx
@@ -1,9 +1,19 @@
-import {NavBarTabProps} from "../../data/Interfaces.ts"
+import { NavBarTabProps } from "../../data/Interfaces.ts"
 import { Typography } from "@mui/material"
+import { BodyState } from "../../data/model.ts"
+
+function SetTextColor(appBodyState: BodyState) {
+  if(appBodyState === BodyState.Overview) {
+    return "navbar-home navbar-txt-selected"
+  }
+  else {
+    return "navbar-home navbar-txt-unselected"
+  }
+}
 
 function NavBarHome(props: NavBarTabProps) {
   return(
-    <div onClick={() => props.setAppBodyState(props.stateToSet)} className={"navbar-home"}>
+    <div onClick={() => props.setAppBodyState(props.stateToSet)} className={SetTextColor(props.appBodyState)}>
       <Typography>
         {props.tabText}
       </Typography>
--- src/components/navbar/NavBarTab.tsx
+++ src/components/navbar/NavBarTab.tsx
@@ -1,9 +1,19 @@
 import {NavBarTabProps} from "../../data/Interfaces.ts"
 import { Typography } from "@mui/material"
+import { BodyState } from "../../data/model.ts"
+
+function SetTextColor(appBodyState: BodyState, stateToSet: BodyState) {
+  if(appBodyState === stateToSet) {
+    return "navbar-tab navbar-txt-selected"
+  }
+  else {
+    return "navbar-tab navbar-txt-unselected"
+  }
+}
 
 function NavBarTab(props: NavBarTabProps) {
   return(
-    <div onClick={() => props.setAppBodyState(props.stateToSet)} className={"navbar-tab"}>
+    <div onClick={() => props.setAppBodyState(props.stateToSet)} className={SetTextColor(props.appBodyState, props.stateToSet)}>
       <Typography>
         {props.tabText}
       </Typography>
